aefc800d8c17ba49cfb8f54972c4a35be9fcc396
Apple <apple@MacBook-Pro-Apple.local>
2024-06-12 19:30:04 +0300

    Добавлено домашнее задание 3

--- lection_3/homework.js
+++ lection_3/homework.js
@@ -1,64 +1,64 @@
 // //1
-// const user = {
-//     name: 'Bob',
-//     funcFunc() {
-//         return function() {
-//             console.log(this);
-//         }
-//     },
-//     funcArrow() {
-//         return () => {
-//             console.log(this);
-//         }
-//     },
-//     arrowFunc: () => {
-//         return function() {
-//             console.log(this);
-//         }
-//     },
-//     arrowArrow: () => {
-//         return () => {
-//             console.log(this);
-//         }
-//     },
-// };
-//
-// user.funcFunc()(); // ?
-// user.funcArrow()(); // ?
-// user.arrowFunc()(); // ?
-// user.arrowArrow()(); // ?
+const user = {
+    name: 'Bob',
+    funcFunc() {
+        return function() {
+            console.log(this);
+        }
+    },
+    funcArrow() {
+        return () => {
+            console.log(this);
+        }
+    },
+    arrowFunc: () => {
+        return function() {
+            console.log(this);
+        }
+    },
+    arrowArrow: () => {
+        return () => {
+            console.log(this);
+        }
+    },
+};
+
+user.funcFunc()(); // {window}
+user.funcArrow()(); // {user}
+user.arrowFunc()(); // {window}
+user.arrowArrow()(); // {window}
 
 // 2
-// var poke1 = {name:'Pikachu'};
-// var poke2 = {name:'Chermander'};
-// var poke3 = {name:'Bulbasaur'};
-//
-// var sayName = function(){ console.log(this.name) }
-//
-// sayName.bind(poke1).bind(poke2).call(poke3);
+var poke1 = {name:'Pikachu'};
+var poke2 = {name:'Chermander'};
+var poke3 = {name:'Bulbasaur'};
+
+var sayName = function(){ console.log(this.name) } 
+
+sayName.bind(poke1).bind(poke2).call(poke3); // 'Pikachu'
 
 
 // 3
-// const obj = {
-//     firstName: 'Bill',
-//     lastName: 'Ivanov',
-//
-//     showFirstName: function () {
-//         console.log(this.firstName);
-//     },
-//
-//     showLastName: () => {
-//         console.log(this.lastName);
-//     }
-// }
-//
-// obj.showFirstName(); // ?
-// obj.showLastName(); // ?=
-//
-// obj.showFirstName.bind({ firstName: 'Boris' })(); // ?
-// obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // ?
-//
-// obj.showLastName.bind({ lastName: 'Boris' })(); // ?
+const obj = {
+    firstName: 'Bill',
+    lastName: 'Ivanov',
+
+    showFirstName: function () {
+        console.log(this.firstName);
+    },
+
+    showLastName: () => {
+        console.log(this.lastName);
+    }
+}
+
+ obj.showFirstName(); // Bill
+ obj.showLastName(); // undefined
+
+ obj.showFirstName.bind({ firstName: 'Boris' })(); // Boris
+ obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // Boris
+
+ obj.showLastName.bind({ lastName: 'Boris' })(); // undefined
 
 // 4
 
@@ -68,12 +68,14 @@
 //         console.log(this.name)
 //     }
 // }
-//
-// setTimeout(user.fn, 1000)
+
+// const fn = user.fn.bind(user)
+
+// setTimeout(fn, 1000)
 
 
 
-// Что будет выведено в консоль после истечения таймаута и почему?
+// Что будет выведено в консоль после истечения таймаута и почему?// будет undefined и потеря контекста
 // Сделайте так, чтоб починить и выводило "Mike"
 
 // Подсказка - ответ найдете в 5 ссылке README
@@ -100,7 +102,7 @@
 //
 // };
 //
-// askPassword(user.loginOk, user.loginFail) //***;
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
 
 
 
